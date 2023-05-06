74d6d3537ed24a5d72867bb8de0e899c6edab142
Rosario De Chiara <rosario.dechiara@posteitaliane.it>
2023-05-06 16:22:21 +0200

    ** add ** added function to generate fake date

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// Define a function to generate a random number between min (inclusive) and max (inclusive)
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Create a new Date object representing today's date
+const today = new Date();
+
+// Create a new Date object representing one year ago from today
+const oneYearAgo = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
+
+// Initialize an empty object to store the random numbers for each day of the year
+const commitsPerDate = {};
+
+function generateFakeCommitData() {
+  for (let date = new Date(oneYearAgo); date <= today; date.setDate(date.getDate() + 1)) {
+    // Format the date as a string in yyyy-mm-dd format
+    const dateString = date.toISOString().substring(0, 10);
+
+    // Generate a random number between 0 and 100
+    const randomNumber = getRandomNumber(0, 100);
+
+    // Add the random number to the object with the date as the key
+    commitsPerDate[dateString] = randomNumber;
+  }
+}
+
 function App() {
+  generateFakeCommitData();
+
   return (
     <div className="App">
       <header className="App-header">
