f416b5fa7653a45a59614cb1408c5ccacfe4caeb
Rosario De Chiara <rosario.dechiara@posteitaliane.it>
2023-05-20 10:12:41 +0200

    ** fix ** rewrote the random number generation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import './App.css';
 import Basic from './components/basic';
-import Heatmap from './components/advanced';
-
-// Define a function to generate a random number between min (inclusive) and max (inclusive)
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Create a new Date object representing today's date
-const today = new Date();
- 
-// Create a new Date object representing one year ago from today
-const oneYearAgo = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
+import Advanced from './components/advanced';
 
 // Initialize an empty object to store the random numbers for each day of the year
 var commitsPerDate = [];
 
 function generateFakeCommitData() {
-  for (let date = new Date(oneYearAgo); date <= today; date.setDate(date.getDate() + 1)) {
-    // Format the date as a string in yyyy-mm-dd format
-    const dateString = date.toISOString().substring(0, 10);
+  for (let month = 0; month < 12; month++) {
+    // Get the number of days in the current month
+    const numberOfDays = new Date(2023, month + 1, 0).getDate();
 
-    // Generate a random number between 0 and 100
-    const randomNumber = getRandomNumber(0, 100);
+    // Loop through each day of the current month
+    for (let day = 1; day <= numberOfDays; day++) {
+      // Create a new Date object for the current day
+      const currentDate = new Date(2023, month, day, 5); // hack: added 5 hours to avoid midnight
 
-    // Add the random number to the object with the date as the key
-    commitsPerDate.push({date:dateString.split('/').join('-'), count: randomNumber});
+      // Generate a random number between 0 and 100
+      const randomNumber = Math.floor(Math.random() * 100);
+
+      console.log(currentDate.toJSON().substring(0, 10))
+
+      // Add a new object to the array
+      commitsPerDate.push({
+        date: currentDate.toJSON().substring(0, 10),
+        count: randomNumber
+      });
+    }
   }
 }
 
@@ -37,8 +37,8 @@ function App() {
       <header className="App-header">
         Heatmaps library for React showdown
       </header>
-      <Basic commitsData = { commitsPerDate } />
-      <Heatmap data = { commitsPerDate } />
+      <Basic commitsData={commitsPerDate} />
+      <Advanced commitsData={commitsPerDate} />
     </div>
   );
 }
